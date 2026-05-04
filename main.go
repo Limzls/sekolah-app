caa707ef9159a704068cc4d35c1bdac1c8a9efd0
lim.zl <michaellim.lim13@gmail.com>
2026-05-04 20:52:49 +0700

    perubahan kode

--- main.go
+++ main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 
-    //"sekolahapp/config"
-    "sekolahapp/handlers"
+	"sekolahapp/handlers"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	//config.ConnectDB()
+	r := mux.NewRouter()
 
-	r := mux.NewRouter() // jika run tidak bisa nonaktifkan keamanan //
-    
-    r.HandleFunc("/", handlers.HomeHandler).Methods("GET")
-    r.HandleFunc("/login", handlers.LoginHandler).Methods("GET", "POST")
+	// routing utama
+	r.HandleFunc("/", handlers.HomeHandler).Methods("GET")
+	r.HandleFunc("/login", handlers.LoginHandler).Methods("GET", "POST")
+
+	// 🔥 TAMBAHAN PENTING
+	r.HandleFunc("/siswa", handlers.GetSiswa).Methods("GET")
 
 	log.Println("Server running on http://localhost:8080")
 	http.ListenAndServe(":8080", r)
